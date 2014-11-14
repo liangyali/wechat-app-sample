073d0430c3c9cc1c03a79d898a92dd8cf0a81143
liangyali <me@liangyali.com>
2014-11-14 19:44:57 +0800

    test

--- controllers/home/index.js
+++ controllers/home/index.js
@@ -1,18 +1,12 @@
 'use strict';
 
 
-var HomeModel = require('../../models/home');
-
-
 module.exports = function (router) {
 
-    var model = new HomeModel();
-
-
     router.get('/', function (req, res) {
-        
-        res.render('home/index', model);
-        
+
+        res.render('home/index', {});
+
     });
 
 };
