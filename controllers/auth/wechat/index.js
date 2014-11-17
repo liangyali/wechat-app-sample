8a346d47c4d8a85f6f81efff8936366845b4c117
liangyali <me@liangyali.com>
2014-11-18 01:03:26 +0800

    test

--- controllers/auth/wechat/index.js
+++ controllers/auth/wechat/index.js
@@ -6,17 +6,12 @@ var passport = require('passport');
 
 module.exports = function (router) {
 
-    /**
-     * 微信认证
-     */
+
     router.get('/', passport.authenticate('wechat'));
 
-    /**
-     * 微信回调处理
-     */
     router.get('/callback', passport.authenticate('wechat', {
         failureRedirect: '/auth/err',
-        successRedirect: '/auth/wechat/success'}));
+        successRedirect: req.session.goingTo || '/profile'}));
 
     router.get('/success', function (req, res) {
         res.json({user: req.user || ''});
